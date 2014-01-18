8d3a6bf8125e130cd7931eb7ebc04af2f6971f14
Azzyild <pixigen@gmail.com>
2014-01-18 07:13:54 +0400

    Hello  world

---
+++ WebApplication5/Default.aspx.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace WebApplication5
+{
+    class OurText
+    {
+        public int id;
+        public string ourtext;
+    }
+
+    public partial class Default : System.Web.UI.Page
+    {
+        ArrayList aL = new ArrayList();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string myConnectionString = "Server=tcp:a1eah8svsr.database.windows.net,1433;Database=DB;User ID=VOVA@a1eah8svsr;Password=1qazse432!;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+            using (SqlConnection connection = new SqlConnection(myConnectionString))
+            {
+                connection.Open();
+                var transaction = connection.BeginTransaction();
+
+                string sqlQuery = "select * from IldTable";
+                SqlCommand command = new SqlCommand(sqlQuery, connection, transaction);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    OurText newitem = new OurText();
+                    newitem.id = reader.GetInt32(0);
+                    newitem.ourtext = reader.GetString(1);
+                    aL.Add(newitem);
+                }
+                reader.Close();
+                transaction.Commit();
+                connection.Close();
+            }
+
+            for (int i = 0; i < aL.Count; i++)
+            {
+                OurText OT = (OurText)aL[aL.Count-i-1];
+
+                Label textLabel = new Label();
+                textLabel.Text = (aL.Count - i) + ". " + OT.ourtext;
+                textLabel.CssClass = "blogCssClass";
+
+                Label stringLabel = new Label();
+                stringLabel.CssClass = "blogCssClass1";
+                stringLabel.Text = "****************************************************************************************************";
+                ContentPlaceHolder.Controls.Add(stringLabel);
+
+                ContentPlaceHolder.Controls.Add(textLabel);
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int id;
+            string myConnectionString = "Server=tcp:a1eah8svsr.database.windows.net,1433;Database=DB;User ID=VOVA@a1eah8svsr;Password=1qazse432!;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+
+            using (SqlConnection connection = new SqlConnection(myConnectionString))
+            {
+                connection.Open();
+                var transaction = connection.BeginTransaction();
+
+                string sqlQuery = "select Id from IldTable";
+                SqlCommand command = new SqlCommand(sqlQuery, connection, transaction);
+                var reader = command.ExecuteReader();
+                id = 1;
+                while (reader.Read())
+                {
+                    id = reader.GetInt32(0)+1;
+                }
+                reader.Close();
+                transaction.Commit();
+                connection.Close();
+            }
+            
+            using (SqlConnection connection = new SqlConnection(myConnectionString))
+            {
+                connection.Open();
+                var transaction = connection.BeginTransaction();
+
+                string sqlQuery = "insert into IldTable (Id, ourtext) values ("+id+","+"'"+TextBox1.Text+"');";
+                SqlCommand command = new SqlCommand(sqlQuery, connection, transaction);
+                var reader = command.ExecuteNonQuery();
+                transaction.Commit();
+                connection.Close();
+            }
+            Page.Response.Redirect("~/Default.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (TextBox2.Text != "")
+            {
+                try
+                {
+                    int number=Convert.ToInt32(TextBox2.Text);
+                    if (aL.Count < number)
+                    {
+                        Label1.Text = "Number that you have typed is not valid.";
+                    }
+                    else
+                    {
+                        OurText OT=(OurText)aL[number-1];
+                        string myConnectionString = "Server=tcp:a1eah8svsr.database.windows.net,1433;Database=DB;User ID=VOVA@a1eah8svsr;Password=1qazse432!;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+                        using (SqlConnection connection = new SqlConnection(myConnectionString))
+                        {
+                            connection.Open();
+                            var transaction = connection.BeginTransaction();
+
+                            string sqlQuery = "delete from IldTable where Id="+OT.id;
+                            SqlCommand command = new SqlCommand(sqlQuery, connection, transaction);
+                            var reader = command.ExecuteNonQuery();
+                            transaction.Commit();
+                            connection.Close();
+                        }
+                        Page.Response.Redirect("~/Default.aspx");
+                    }
+                }
+                catch
+                {
+                    Label1.Text = "You must type the number, not letters!";
+                }
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+         {
+            if (TextBox4.Text != "")
+            {
+                try
+                {
+                    int number=Convert.ToInt32(TextBox4.Text);
+                    if (aL.Count < number)
+                    {
+                        Label2.Text = "Number that you have typed is not valid.";
+                    }
+                    else
+                    {
+                        OurText OT = (OurText)aL[number - 1];
+                        string myConnectionString = "Server=tcp:a1eah8svsr.database.windows.net,1433;Database=DB;User ID=VOVA@a1eah8svsr;Password=1qazse432!;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+                        using (SqlConnection connection = new SqlConnection(myConnectionString))
+                        {
+                            connection.Open();
+                            var transaction = connection.BeginTransaction();
+
+                           string sqlQuery = "update IldTable set OurText='"+TextBox3.Text+"' Where Id=" + OT.id+';';
+                            SqlCommand command = new SqlCommand(sqlQuery, connection, transaction);
+                            var reader = command.ExecuteNonQuery();
+                            transaction.Commit();
+                            connection.Close();
+                        }
+                        Page.Response.Redirect("~/Default.aspx");
+                    }
+                }
+                catch
+                {
+                    Label2.Text = "You must type the number, not letters!";
+                }
+            }
+        }
+
+        }
+    }
---
+++ WebApplication5/obj/Debug/Package/PackageTmp/Default.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication5.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+    <link rel='stylesheet' type="text/css" href="StyleSheet1.css" />
+</head>
+
+
+
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label3" runat="server" Text="Блог разработки блога" Font-Bold="true" Font-Size="22px"></asp:Label>
+
+        <div class="denide"></div>
+
+        <div class="divClass">
+            <asp:TextBox ID="TextBox1" runat="server" TextMode="MultiLine" Rows="5" Width="200" Text="type your minds..."></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Create" onclick="Button1_Click" />
+        </div>
+
+        <div class="denide"></div>
+
+        <div class="divClass">
+            <asp:Label ID="Label1" runat="server" Text="Type the number of post you want to delete:"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server" Width="20"></asp:TextBox>
+            <asp:Button ID="Button2" runat="server" Text="Delete" onclick="Button2_Click" />
+        </div>
+
+        <div class="denide"></div>
+
+        <div class="divClass">
+            <asp:Label ID="Label2" runat="server" Text="To change any post type new text in the large textbox and the number in the small textbox below."></asp:Label>
+            <div class="divClass">
+                <asp:TextBox ID="TextBox3" runat="server" TextMode="MultiLine" Width="200" Rows="5"></asp:TextBox>
+                <asp:TextBox ID="TextBox4" runat="server" Width="20"></asp:TextBox>
+                <asp:Button ID="Button3" runat="server" Text="Update" onclick="Button3_Click" />
+            </div>
+        </div>
+
+        <div class="denide"></div>
+
+        <div class="divClass">
+            <asp:PlaceHolder ID="ContentPlaceHolder" runat="server"></asp:PlaceHolder>
+        </div>
+
+    </div>
+    </form>
+</body>
+</html>
